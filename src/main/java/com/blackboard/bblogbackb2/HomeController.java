e5e4ccda1ea81541ad53edf9823e717ad34df11e
markkauffman2000 <mkauffman@mkauffmanmbp.local>
2017-04-28 10:38:20 -0700

    version 1.8.0 - fixed file permissions - only binding to logback now

--- src/main/java/com/blackboard/bblogbackb2/HomeController.java
+++ src/main/java/com/blackboard/bblogbackb2/HomeController.java
@@ -12,10 +12,10 @@ package com.blackboard.bblogbackb2;
  *     notice, this list of conditions and the following disclaimer in the
  *     documentation and/or other materials provided with the distribution.
  *
- *  -- Neither the name of Blackboard Inc. nor the names of its contributors 
- *     may be used to endorse or promote products derived from this 
+ *  -- Neither the name of Blackboard Inc. nor the names of its contributors
+ *     may be used to endorse or promote products derived from this
  *     software without specific prior written permission.
- *  
+ *
  * THIS SOFTWARE IS PROVIDED BY BLACKBOARD INC ``AS IS'' AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -28,6 +28,9 @@ package com.blackboard.bblogbackb2;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+
+ import ch.qos.logback.classic.LoggerContext;
+ import ch.qos.logback.core.util.StatusPrinter;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -47,60 +50,64 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info("Welcome to /! The client locale is {}.", locale);
 		logger.error("There was NOT an error. This is a test message.");
 		System.out.println("springmvcb2 - Is this visible in Kibana? I didn't intentionally log it in the correct format.");
 
 		Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
+
 		model.addAttribute("serverTime", formattedDate );
-		
+
 		return "home";
 	}
 
 	@RequestMapping(value = "/learnhello", method = RequestMethod.GET)
 	public String learnhello(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
+		logger.info("Welcome to /learnhello! The client locale is {}.", locale);
+
+		// print internal Logback state
+		LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+		StatusPrinter.print(lc);
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
+
 		model.addAttribute("serverTime", formattedDate );
-		
+
 		return "learnhello";
 	}
-	
-        
+
+
         @RequestMapping(value = "/logbacklog", method = RequestMethod.GET)
 	public String logbacklog(Locale locale, Model model) {
 		// MyLogger.info("Using Learn Logger! The client locale is {}.", locale);
-		logger.info("Welcome home from /learnlog! The client locale is {}.", locale);
+		logger.info("Welcome to /logbacklog! The client locale is {}.", locale);
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
+
 		model.addAttribute("serverTime", formattedDate );
-		
+
                 MybbLOGGER.getBbLogger().logInfo(formattedDate + "  Info from logbacklog" );
                 MybbLOGGER.getBbLogger().logError(formattedDate + "  Error from logbacklog, not a real Error" );
                 MybbLOGGER.getBbLogger().logDebug(formattedDate + "  Debug from logbacklog" );
-                
+
 		return "logbacklog";
 	}
 }
